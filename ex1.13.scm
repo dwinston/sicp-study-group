9a9df4e410290b190ec1694702a813a32bd0e492
Donny Winston <dwinston@alum.mit.edu>
2014-06-21 08:43:42 -0700

    Fib(n) is closest Int to (GoldenRatio)^n/sqrt(5)

---
+++ ex1.13.scm
@@ -0,0 +1,8 @@
+;; Want to show (= (fib n) (/ (- (pow ϕ n) (pow ψ n)) (sqrt 5)))
+
+;; First, show by inspection that the equation holds for n=0 and n=1
+
+;; Then, assume truth of the formula and show that the presumed expression for
+;; (fib (+ n 1)) is correct. I found this to be true.
+
+;; A snaphot of my (disorganized) work is in ex1.13.jpg
